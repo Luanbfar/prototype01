019b91eb3d5b29cc439c79a4413b59e886bf6dd8
Luanbfar <luanbfarias@outlook.com>
2024-04-10 07:19:33 -0300

    Fixed small issues and added audio

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var currentPagePath = window.location.pathname;
 
-var links = document.querySelectorAll(".links a");
+var links = document.querySelectorAll(".header-links a");
 
 links.forEach(function (link) {
   var linkPath = new URL(link.href).pathname;
